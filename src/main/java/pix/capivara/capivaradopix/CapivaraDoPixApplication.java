e1bb01fb223be5e5e94173e0f949b1ae87cb3c52
Renan Araújo <awkrenan@gmail.com>
2023-07-10 10:21:23 -0300

    setup spring

---
+++ src/main/java/pix/capivara/capivaradopix/CapivaraDoPixApplication.java
@@ -0,0 +1,13 @@
+package pix.capivara.capivaradopix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapivaraDoPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapivaraDoPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pix/capivara/capivaradopix/CapivaraDoPixApplicationTests.java
@@ -0,0 +1,13 @@
+package pix.capivara.capivaradopix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapivaraDoPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
